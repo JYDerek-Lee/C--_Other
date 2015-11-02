2d5a9f6fdfa5157a00cae4951114b1ad19045f8b
JuneYoungs <asplannedl@gmail.com>
2015-11-02 17:05:50 +0900

    3차 과제 완료

---
+++ Assignment3/code1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const double pi = 3.14159;
+
+class Point {
+public:
+	Point(int x = 0, int y = 0) :px(x), py(y) {
+
+	}
+	void showPoint() const {
+		cout << "x = " << px << ", y = " << py << endl;
+	}
+	~Point() {
+	}
+
+private:
+	friend class Circle;
+	int px;
+	int py;
+};
+
+class Circle {
+public:
+	Circle(Point cen, int rad = 0) :center(cen), radius(rad) {
+		color = "black";
+	}
+	~Circle() {
+	}
+	void getArea() {
+		area = pi*radius*radius;
+	}
+	void showCircle() {
+		cout << "중심점: (x = "<< center.px << ", y = " << center.py << ")";
+		cout << " 반지름 : " << radius << " 색상 : " << color << endl;
+	}
+	void showArea(Circle &cir) {
+		cout << "원의 넓이 : " << cir.area << endl;
+	}
+
+private:
+	Point center;
+	int radius;
+	string color;
+	double area;
+};
+
+void main() {
+	Point cen(5, 7);
+	Circle cir1(cen, 10);
+
+	cir1.showCircle();
+	cir1.getArea();
+	cir1.showArea(cir1);
+}
